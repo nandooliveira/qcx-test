13deffb60c36c01bf01518da409f10c7e3d076bc
Fernando Oliveira <nandooliveira.al@gmail.com>
2020-03-21 20:58:47 -0300

    Handle Webhook UseCase

--- lib/core/webhooks/use_cases/handle.rb
+++ lib/core/webhooks/use_cases/handle.rb
@@ -1,32 +1,40 @@
 module Core
   module Webhooks
     module UseCases
-      class Handle
-        def initialize(event_type:, params:)
-          @event_type = event_type
-          @params     = params
-
-          validate_params
-        end
+      class Handle < ::Core::BaseUseCase
+        option :headers, Types::Hash, reader: :private
+        option :params,  Types::Hash, reader: :private
 
         def call
           handle_webhook
         end
 
-        attr_reader :event_type, :params
-
         def handle_webhook
           service_class = "::Core::Webhooks::Services::Handle#{event_type.camelize}WebhookEvent".constantize
-          service_class.new(params: params).call
+          service_class.new(service_params).call
         rescue NameError
-          { message: "Unknown event \"#{event_type}\"!" }
+          ::Core::Webhooks::Services::BaseHandleWebhookEvent.new(service_params).call
         end
 
-        def validate_params
-          raise ::Core::Exceptions::ArgumentError, 'Parameter "event_type" is invalid!' \
-            unless event_type.is_a?(::String)
-          raise ::Core::Exceptions::ArgumentError, 'Parameter "params" is invalid!' \
-            unless params.is_a?(::Hash)
+        def service_params
+          {
+            event_type: event_type,
+            delivery:   delivery,
+            signature:  signature,
+            payload:    params,
+          }
+        end
+
+        def event_type
+          @event_type ||= headers['X-Github-Event']
+        end
+
+        def delivery
+          headers['X-Github-Delivery']
+        end
+
+        def signature
+          headers['X-Hub-Signature']
         end
       end
     end
--- spec/core/webhooks/use_cases/handle_spec.rb
+++ spec/core/webhooks/use_cases/handle_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 describe ::Core::Webhooks::UseCases::Handle do
   describe '.new' do
-    context 'invalid event_type' do
+    context 'invalid headers' do
       it 'raise error' do
-        [1, nil, {}, []].each do |event_type|
-          expect { described_class.new(event_type: event_type, params: {}) }.to raise_error(
-            ::Core::Exceptions::ArgumentError,
-            'Parameter "event_type" is invalid!'
+        [1, nil, '', []].each do |headers|
+          expect { described_class.new(headers: headers, params: {}) }.to raise_error(
+            Dry::Types::ConstraintError
           )
         end
       end
@@ -16,9 +15,8 @@ describe ::Core::Webhooks::UseCases::Handle do
     context 'invalid params' do
       it 'raise error' do
         [1, nil, '', []].each do |params|
-          expect { described_class.new(event_type: 'issues', params: params) }.to raise_error(
-            ::Core::Exceptions::ArgumentError,
-            'Parameter "params" is invalid!'
+          expect { described_class.new(headers: 'issues', params: params) }.to raise_error(
+            Dry::Types::ConstraintError
           )
         end
       end
@@ -27,44 +25,67 @@ describe ::Core::Webhooks::UseCases::Handle do
 
   describe '#call' do
     subject(:handle_event) do
-      described_class.new(event_type: event_type, params: params).call
+      described_class.new(headers: headers, params: params).call
     end
 
+    let(:delivery)   { '89ec1780-6bc2-11ea-8f7a-bd2696ebe5aa' }
+    let(:event_type) { 'ping' }
+    let(:signature)  { 'sha1=12518a93a2cdb9f54ed8bd8af5eac45b24871f49' }
+    let(:headers) do
+      {
+        'X-GitHub-Delivery' => delivery,
+        'X-GitHub-Event'    => event_type,
+        'X-Hub-Signature'   => signature,
+      }
+    end
     let(:params) { {} }
 
     context 'unknown event' do
       let(:event_type) { 'unknown_event' }
 
-      it 'return a message' do
-        expect(handle_event).to match(message: "Unknown event \"#{event_type}\"!")
+      it 'create using the generic service' do
+        expect { handle_event }.to change(::Event, :count).by(1)
+      end
+
+      it 'create with correct data' do
+        expect(handle_event.event_type).to eql(event_type)
+        expect(handle_event.delivery).to eql(delivery)
+        expect(handle_event.signature).to eql(signature)
       end
     end
 
     context 'any known event' do
-      module Core
-        module Webhooks
-          module Services
-            class HandleKnownEventWebhookEvent
-              def initialize(params:); end
-            end
-          end
+      let(:event_type) { 'ping' }
+
+      it 'create a event on database' do
+        expect { handle_event }.to change(::Event, :count).by(1)
+      end
+
+      it 'create with correct data' do
+        expect(handle_event.event_type).to eql(event_type)
+        expect(handle_event.delivery).to eql(delivery)
+        expect(handle_event.signature).to eql(signature)
+      end
+
+      context 'check if correct class is being called' do
+        let(:event_service)          { ::Core::Webhooks::Services::HandlePingWebhookEvent }
+        let(:event_service_instance) { spy }
+
+        before do
+          allow(event_service).to receive(:new).and_return(event_service_instance)
         end
-      end
 
-      let(:event_type)             { 'known_event' }
-      let(:event_service)          { ::Core::Webhooks::Services::HandleKnownEventWebhookEvent }
-      let(:event_service_instance) { double }
+        it 'call the service passing the params' do
+          handle_event
 
-      before do
-        allow(event_service).to receive(:new).with(params: params).and_return(event_service_instance)
-        allow(event_service_instance).to receive(:call)
-      end
-
-      it 'call the service passing the params' do
-        handle_event
-
-        expect(event_service).to have_received(:new).with(params: params)
-        expect(event_service_instance).to have_received(:call)
+          expect(event_service).to have_received(:new).with(
+            event_type: event_type,
+            delivery:   delivery,
+            signature:  signature,
+            payload:    params
+          )
+          expect(event_service_instance).to have_received(:call)
+        end
       end
     end
   end
